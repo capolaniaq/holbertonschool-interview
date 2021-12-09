b529977747ecb8cbfdd4add57b47c081c0772150
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-09 10:55:32 -0500

    0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This script is used to generate lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    This function checks if the boxes can be unlocked
+    """
+    for x, box in enumerate(boxes):
+        if len(box) == 0 and x != len(boxes) - 1:
+            return False
+        if x == len(boxes) - 1:
+            return True
