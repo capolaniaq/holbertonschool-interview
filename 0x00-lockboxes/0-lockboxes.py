89a88152b25fce35a3810e86c8e6eb483e2c017a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-09 11:02:26 -0500

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,7 +8,11 @@ def canUnlockAll(boxes):
     """
     This function checks if the boxes can be unlocked
     """
+    if type(boxes) is not list:
+        return False
     for x, box in enumerate(boxes):
+        if type(box) is not list:
+            return False
         if len(box) == 0 and x != len(boxes) - 1:
             return False
         if x == len(boxes) - 1:
