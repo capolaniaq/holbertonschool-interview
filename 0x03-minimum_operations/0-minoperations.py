f6aa931d7da16c245a735e809068ce6fc84f06cc
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-05 13:58:57 -0500

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -13,7 +13,7 @@ def minOperations(n):
     elif n < 0:
         return 0
     if n == 1:
-        return 1
+        return 0
     if n % 2 == 0 and n != 2:
         return minOperations(int(n / 2)) + 2
     elif n % 3 == 0 and n != 3:
