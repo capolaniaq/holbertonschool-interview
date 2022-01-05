6dec1f9b402aa468b76c7d68b023871429d24878
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-05 13:53:40 -0500

    0-minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Minium steps to reach a target number
+"""
+
+
+def minOperations(n):
+    """
+    Minimum Operations functions
+    """
+    if type(n) is not int:
+        return 0
+    if n == 1:
+        return 1
+    if n % 2 == 0 and n != 2:
+        return minOperations(int(n / 2)) + 2
+    elif n % 3 == 0 and n != 3:
+        return minOperations(int(n / 3)) + 3
+    else:
+        return n
