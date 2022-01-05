e16ae07ea8ecfd1740ad6fda983618a545aa7139
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-05 14:21:56 -0500

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,13 +9,13 @@ def minOperations(n):
     """
     if not isinstance(n, int):
         return 0
-    if n < 0 or n == 1:
-        return 0
-    if n % 2 == 0 and n != 2:
-        return minOperations(int(n / 2)) + 2
-    elif n % 3 == 0 and n != 3:
-        return minOperations(int(n / 3)) + 3
-    elif n % 5 == 0 and n != 5:
-        return minOperations(int(n / 5)) + 5
-    else:
-        return n
+
+    op = 0
+    i = 2
+    while (i <= n):
+        if not (n % i):
+            n = int(n / i)
+            op += i
+            i = 1
+        i += 1
+    return op
