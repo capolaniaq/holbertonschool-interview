ba67b137ead578a1d18b20e37c098804d2322920
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-13 02:12:11 -0500

    0-sandpiles.c

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -12,15 +12,121 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 
-	if (grid1 == NULL || grid2 == NULL)
-		return;
-
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
 		}
+	}
+	printf("=\n");
+	my_print_grid(grid1);
+	if (grid1[0][0] > 3 || grid1[0][1] > 3 || grid1[0][2] > 3 ||
+		grid1[1][0] > 3 || grid1[1][1] > 3 || grid1[1][2] > 3 ||
+		grid1[2][0] > 3 || grid1[2][1] > 3 || grid1[2][2] > 3)
+	{
+		stables_sandpiles(grid1);
+	}
+}
+
+/**
+ * stable_sandpiles - checks if a sandpile is stable
+ * @grid1: first sandpile
+ *
+ * Return: void
+ */
+
+void stables_sandpiles(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if (i == 0 && j == 0)
+				{
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+				}
+				else if (i == 0 && j == 1)
+				{
+					grid1[i][j -1] = grid1[i][j -1] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+				}
+				else if (i == 0 && j == 2)
+				{
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+				}
+				else if (i == 1 && j == 0)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+				}
+				else if (i == 1 && j == 1)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+				}
+				else if (i == 1 && j == 2)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+					grid1[i + 1][j] = grid1[i + 1][j] + 1;
+				}
+				else if (i == 2 && j == 0)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+				}
+				else if (i == 2 && j == 1)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+					grid1[i][j + 1] = grid1[i][j + 1] + 1;
+				}
+				else if (i == 2 && j == 2)
+				{
+					grid1[i - 1][j] = grid1[i - 1][j] + 1;
+					grid1[i][j - 1] = grid1[i][j - 1] + 1;
+				}
+			}
+
+		}
 
 	}
+
+	if (grid1[0][0] > 3 || grid1[0][1] > 3 || grid1[0][2] > 3 ||
+		grid1[1][0] > 3 || grid1[1][1] > 3 || grid1[1][2] > 3 ||
+		grid1[2][0] > 3 || grid1[2][1] > 3 || grid1[2][2] > 3)
+		{
+			printf("=\n");
+			my_print_grid(grid1);
+			stables_sandpiles(grid1);
+		}
+}
+
+
+void my_print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -12,6 +12,9 @@
  *
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void stables_sandpiles(int grid1[3][3]);
+void my_print_grid(int grid[3][3]);
+
 
 
 
