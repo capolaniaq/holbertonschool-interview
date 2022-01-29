05e6f4b27e088b99eef84d6d72895268c3f51f4e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-29 17:10:49 -0500

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,40 +2,36 @@
 """
 Script that reads stdin line by line and computes metrics:
 """
-import signal
+
+
 import sys
-import re
+
 
 if __name__ == "__main__":
 
-    import signal
-    import sys
-    import re
-
+    status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+                   405: 0, 500: 0}
+    file_size = 0
+    total_lines = 0
 
     def print_values(status_code={}, file_size=0):
         """
         Prints the status code and the file size
         """
-        print('File size: {}'.format(file_size))
+        print("File size: {}".format(file_size))
         for key, value in status_code.items():
             if value != 0:
-                print('{}: {}'.format(key, value))
+                print("{}: {}".format(key, value))
+
     try:
-        k = 0
-        valid = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET /projects/260 HTTP/1.1" \d{3} \d{1,5}$')
-        status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-        file_size = 0
         for line in sys.stdin:
-            if valid.match(line):
-                separator = line.split(' ')
-                status_code[int(separator[-2])] += 1
-                file_size = file_size + int(separator[-1])
-                k += 1
-                if k % 10 == 0:
-                    print_values(status_code, file_size)
-    except KeyboardInterrupt:
+            separator = line.split(' ')
+            status_code[int(separator[-2])] += 1
+            file_size = file_size + int(separator[-1])
+            total_lines += 1
+            if total_lines % 10 == 0:
+                print_values(status_code, file_size)
+
+    except (KeyboardInterrupt, SystemExit):
         print_values(status_code, file_size)
-        sys.exit(0)
-
-
+        raise
