5838889bf2b0c23bb738153aeb28903ab7c1afd1
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-30 14:48:41 -0500

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
-"""
+
+""" script that reads stdin line by line and computes metrics """
 
 import sys
 
 
-if __name__ == "__main__":
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
-                   "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    total_lines = 0
 
-    def print_values(status_code, file_size):
-        """
-        Prints the status code and the file size
-        """
-        print("File size: {:d}".format(file_size))
-        for key in sorted(status_code.keys()):
-            if status_code[key] != 0:
-                print("{}: {:d}".format(key, status_code[key]))
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-    try:
-        for line in sys.stdin:
-            if total_lines != 0 and total_lines % 10 == 0:
-                print_values(status_code, file_size)
-            total_lines += 1
-            ln = line.split()
+count = 0
+size = 0
 
-            try:
-                file_size += int(ln[-1])
-            except ValueError:
-                pass
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-            try:
-                status_code[ln[-2]] += 1
-            except ValueError:
-                pass
+        stlist = line.split()
+        count += 1
 
-        print_values(status_code, file_size)
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
 
-    except KeyboardInterrupt:
-        print_values(status_code, file_size)
-        raise
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
