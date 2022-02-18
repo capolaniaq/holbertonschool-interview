3627bfb6021bb75f6fbafeb9de882ed67494e521
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-02-17 23:23:10 -0500

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,19 +4,29 @@ UTF-8 Validation
 """
 
 
-from traceback import print_tb
-
-
 def validUTF8(data):
     """
     data is a list of integers
     Return: True if data is a valid UTF-8
     encoding, else return False
     """
-    for d in data:
-        if d >= 128:
-            try:
-                d.to_bytes(1, 'big')
-            except:
+    byte_count = 0
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0:
+                byte_count = 0
+            else:
                 return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    if byte_count != 0:
+        return False
     return True
