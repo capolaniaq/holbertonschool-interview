58918d531e3f969394ec7052d2978e256422011b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-29 03:31:20 -0500

    0-sorted_array_to_avl.c

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -12,24 +12,40 @@
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	avl_t *tree;
-	size_t i;
+	size_t middle = size / 2, i, middle_m = (middle / 2) + 1;
+	int idx = 0;
 
 	if (!array || !size)
 		return (NULL);
 
 	tree = malloc(sizeof(avl_t));
-
 	if (!tree)
 		return (NULL);
-
-	tree->n = array[0];
+	tree->n = array[middle];
 	tree->parent = NULL;
 	tree->left = NULL;
 	tree->right = NULL;
 
 	for (i = 1; i < size; i++)
 	{
-		insert_node(&tree, array[i]);
+
+		if (i == 1)
+			idx = middle - middle_m;
+		else if (i == 2)
+			idx = middle + middle_m;
+		else if (i == 3)
+			idx = middle - (middle_m + (middle_m / 2));
+		else if (i == 4)
+			idx = middle - (middle_m / 2);
+		else if (i == 5)
+			idx = middle + (middle_m / 2);
+		else if (i == 6)
+			idx = middle + (middle_m + (middle_m / 2));
+		else if (i == 7)
+			idx = middle - middle;
+		else
+			idx = idx + 2;
+		insert_node(&tree, array[idx]);
 	}
 
 	return (tree);
@@ -68,6 +84,19 @@ void insert_node(avl_t **tree, int value)
 		current->right = new_node;
 	else if (current->left->left == NULL || current->left->right == NULL)
 		insert_node(&current->left, value);
-	else
+	else if (current->right->left == NULL || current->right->right == NULL)
 		insert_node(&current->right, value);
+	else if (current->left->left->left == NULL ||
+			 current->left->left->right == NULL)
+		insert_node(&current->left->left, value);
+	else if (current->left->right->left == NULL ||
+			 current->left->right->right == NULL)
+		insert_node(&current->left->right, value);
+	else if (current->right->left->left == NULL ||
+			 current->right->left->right == NULL)
+		insert_node(&current->right->left, value);
+	else if (current->right->right->left == NULL ||
+			 current->right->right->right == NULL)
+		insert_node(&current->right->right, value);
 }
+
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -27,7 +27,7 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 
-void binary_tree_print(const binary_tree_t *);
+void binary_tree_print(const binary_tree_t *tree);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 void insert_node(avl_t **tree, int value);
 
