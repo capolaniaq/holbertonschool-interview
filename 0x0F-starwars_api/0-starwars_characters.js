c6966236282c17da87758ff7fa784d35235a79be
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-06 17:52:38 -0500

    0-starwars_characters.js

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+	for (let i = 0; i < characters.length;  i++){
+		request.get(characters[i],function(error, response, body){
+			if (error) {
+				console.log(error);
+			}else {
+				const character = JSON.parse(body);
+				console.log(character.name);
+			}
+		} );
+	}
+  }
+});
