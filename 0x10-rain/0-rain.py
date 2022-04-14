41633d300fe8f467eb4778c842ee4e4254ba80a9
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-14 12:59:52 -0500

    0-rain.py

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Rain exercise
+"""
+
+
+def rain(walls):
+    """
+    Rain function
+    """
+    if not walls or len(walls) == 0:
+        return 0
+    if [x for x in walls if x < 0]:
+        return 0
+    rain = 0
+    low_wall = 0
+
+    for i in range(1, len(walls)):
+        if walls[i] == 0:
+            continue
+        elif walls[low_wall] <= walls[i]:
+            rain = rain + (walls[low_wall]) * (i - low_wall - 1)
+            low_wall = i
+        else:
+            rain = rain + (walls[i]) * (i - low_wall - 1)
+            low_wall = i
+    return rain
