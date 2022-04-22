ef61d8bbc26e952c3b04eb853b141e6399f2e4f1
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-21 19:01:51 -0500

    sort.h

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -10,10 +10,10 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void swap(int *array, size_t i, size_t child);
+void swap(int *array, size_t i, size_t child, size_t total_size);
 int heapify_down(int *array, size_t size);
-void shift_down(int *array, size_t size);
-int check_array(int *array, size_t size);
+void shift_down(int *array, size_t size, size_t total_size);
+int check_array(int *array, size_t total_size);
 
 
 
