935e34e18cc8df0afa7d07693e2541982be2cd34
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-27 18:45:59 -0500

    0-advanced_binary.c

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int binarySearch(int *arr, size_t low, size_t r, int x);
+void print_array(int *array, size_t low, size_t size);
+
+
+
+#endif /* _SEARCH_ALGOS_H_ */
