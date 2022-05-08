816d5f80c2231d553113f19062b71b2d0d266f7d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-07 23:37:17 -0500

    0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,20 +5,29 @@ Count words in Reddit Api
 import requests
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, hot_dict={}, after=None, count=0):
     """
     request to reddit api and count the number of words
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after is None:
+        [hot_dict.update({x.lower(): 0}) for x in word_list]
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                                 after)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print("None")
         return
+
     data = response.json()
-    count = 0
-    for post in data['data']['children']:
-        for word in word_list:
-            if word in post['data']['title'].lower():
-                count += 1
-    print(count)
+    after = data['data']['after']
+
+    for word in word_list:
+        if word in data['data']['children'][count]['data']['title'].lower():
+            hot_dict[word] += 1
+    count += 1
+
+    if count < len(data['data']['children']):
+        count_words(subreddit, word_list, hot_dict, after, count)
+    else:
+        for key, value in hot_dict.items():
+            print('{}: {}'.format(key, value))
