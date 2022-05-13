dd19bb17ba1438fa5988ec2b018bc092f2138c47
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-12 19:10:09 -0500

    binary_trees.h

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -16,16 +16,18 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
 int heap_extract(heap_t **root);
-
+heap_t *sort_values(heap_t **root);
+heap_t *find_last_leaf(heap_t **root);
+size_t binary_tree_height(heap_t **root);
 
 #endif
