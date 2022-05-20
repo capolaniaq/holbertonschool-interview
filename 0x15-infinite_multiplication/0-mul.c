9c2077232a9bd075f959e5d55d1ea0c6c60c3578
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-19 22:02:53 -0500

    0-mul.c

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,72 +1,148 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * initDigitArray - allocates and sets to 0 an array to contain the digits
+ *   of a base 10 number
  *
- * Return: 0 on success
+ * @size: array size
+ * Return: pointer to initialized array, or NULL on failure
  */
-
-
-int main(int argc, char const *argv[])
+unsigned int *initDigitArray(size_t size)
 {
-	int mul = 0;
-	char num1, num2;
+	unsigned int *arr = NULL;
+	size_t i;
 
-	if (argc != 3)
+	arr = malloc(sizeof(unsigned int) * size);
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = 0;
+
+	return (arr);
+}
+
+
+/**
+ * stringIntMultiply - TBD
+ *
+ * @prod_digits: array to store digits of product
+ * @n1_digits: string containing multiplicand digits in ASCII
+ * @n2_digits: string containing multiplier digits in ASCII
+ * @n1_len: amount of digits in multiplicand
+ * @n2_len: amount of digits in multiplier
+ */
+void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
+		       char *n2_digits, size_t n1_len, size_t n2_len)
+{
+	int i, j, sum;
+	unsigned char digit1, digit2;
+
+	if (prod_digits == NULL || n1_digits == NULL || n2_digits == NULL)
+		return;
+
+	for (i = n1_len - 1; i >= 0; i--)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
+		sum = 0;
+		digit1 = n1_digits[i] - '0';
+
+		for (j = n2_len - 1; j >= 0; j--)
+		{
+			digit2 = n2_digits[j] - '0';
+
+			sum += prod_digits[i + j + 1] + (digit1 * digit2);
+
+			prod_digits[i + j + 1] = sum % 10;
+
+			sum /= 10;
+		}
+
+		if (sum > 0)
+			prod_digits[i + j + 1] += sum;
+	}
+}
+
+
+/**
+ * stringIsPosInt - validates if string represents a positive integer
+ *
+ * @s: string to test
+ * Return: 1 if true, 0 if false
+ */
+int stringIsPosInt(char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 	}
 
-	num1 = argv[1];
-	num2 = argv[2];
+	return (1);
+}
 
-	mul = num1 * num2;
 
-	if (mul % 10 == 0)
-	{
-		_printf_to_mul(mul);
+/**
+ * error - error return
+ *
+ * @status: error code to exit with
+ */
+void error(int status)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(status);
+}
+
+
+/**
+ * main - entry point
+ *
+ * @argc: number of commmand line arguments
+ * @argv: array of commmand line arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char **argv)
+{
+	size_t i, av1_len, av2_len, prod_len;
+	unsigned int *prod_digits = NULL;
+
+	if (argc != 3 || !stringIsPosInt(argv[1]) ||
+	    !stringIsPosInt(argv[2]))
+		error(98);
+
+	for (i = 0, av1_len = 0; argv[1][i]; i++)
+		av1_len++;
+
+	for (i = 0, av2_len = 0; argv[2][i]; i++)
+		av2_len++;
+
+	prod_len = av1_len + av2_len;
+	prod_digits = initDigitArray(prod_len);
+	if (prod_digits == NULL)
+		error(98);
+
+	stringIntMultiply(prod_digits, argv[1], argv[2], av1_len, av2_len);
+
+	/* omit leading zeroes */
+	for (i = 0; !prod_digits[i] && i < prod_len; i++)
+	{}
+
+	if (i == prod_len)
 		_putchar('0');
-	}
-	else
-	{
-		_printf_to_mul(mul);
-	}
+
+	for (; i < prod_len; i++)
+		_putchar(prod_digits[i] + '0');
 	_putchar('\n');
 
+	free(prod_digits);
+
+	return (0);
 }
-
-/**
- * print - prints the result of the multiplication
- * @mul: the result of the multiplication
- *
- * Return: void
- */
-
-void _printf_to_mul(int mul)
-{
-	int rev = 0;
-
-	while (mul > 0)
-	{
-		rev = rev * 10;
-		rev = rev + (mul % 10);
-		mul = mul / 10;
-	}
-
-	while (rev > 0)
-	{
-		_putchar(rev % 10 + '0');
-		rev = rev / 10;
-	}
-
-}
-
--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -1,7 +1,18 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+/* size_t */
+#include <stddef.h>
+
+/* _putchar.c */
 int _putchar(char c);
-void _printf_to_mul(int mul);
 
-#endif
+/* 0-mul.c */
+unsigned int *initDigitArray(size_t size);
+void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
+		       char *n2_digits, size_t n1_len, size_t n2_len);
+int stringIsPosInt(char *s);
+void error(int status);
+/* int main(int argc, char **argv) */
+
+#endif /* HOLBERTON_H */
