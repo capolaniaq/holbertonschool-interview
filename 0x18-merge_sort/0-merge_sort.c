0181730fafde9e0c4b21b0e994304e98982b5bb1
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-06-09 22:54:47 -0500

    0-merge_sort.c

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -8,27 +8,8 @@
  * Return: void
  */
 
-
 void merge_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	//merge(array, size, 0, size - 1);
 }
-
-/**
- * merge - Function that merges two sorted arrays
- * @array: The array to be sorted
- * @size: Number of elements in @array
- * @start: Start index of the array
- * @end: End index of the array
- *
- * Return: void
- */
-
-/*void merge(int *array, size_t size, size_t start, size_t end)
-{
-
-
-}*/
-
--- 0x18-merge_sort/README.md
+++ 0x18-merge_sort/README.md
@@ -91,4 +91,3 @@ Merging...
 7, 13, 19, 48, 52, 71, 73, 86, 96, 99
 alexa@ubuntu-xenial:0x18-merge_sort$
 ```
-
