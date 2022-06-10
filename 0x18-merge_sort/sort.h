29331af0c36d076d5d6e65b95f857b45305aec51
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-06-09 22:58:06 -0500

    0-merge_sort.c

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -9,10 +9,9 @@
 
 void print_array(int *array, int size);
 void merge_sort(int *array, size_t size);
-void merge(int *array, size_t size, size_t start, size_t end);
+//void merge(int *array, size_t size, size_t start, size_t end);
 
 
 
 
-#endif // _SORT_H_
-
+#endif
