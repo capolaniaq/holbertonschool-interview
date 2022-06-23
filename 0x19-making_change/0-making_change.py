b34269983e04befc49d64388833905ca3a760963
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-06-22 22:06:25 -0500

    0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -20,11 +20,10 @@ def makeChange(coins, total):
     count_number = 0
     i = 0
     while i < len(coins):
-        if number + coins[i] <= total:
+        while number + coins[i] <= total:
             number += coins[i]
             count_number += 1
-        else:
-            i += 1
+        i += 1
 
     if number != total:
         return -1
