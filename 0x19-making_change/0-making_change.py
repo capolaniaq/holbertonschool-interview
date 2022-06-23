c5c2d81f42e35b44186afd144b6325c225b5e825
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-06-22 19:24:54 -0500

    0-making_change.py

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+make change
+"""
+
+from audioop import reverse
+
+
+def makeChange(coins, total):
+    """
+    Args:
+        coins is a list of the values of the coins in your possesion
+        total the value to complete
+    Returns:
+         fewest number of coins needed to meet total
+    """
+    if total == 0 or total < 0:
+        return 0
+    if sum(coins) > total:
+        return -1
+
+    coins.sort(reverse=True)
+    number = 0
+    count_number = 0
+    i = 0
+    while i < len(coins):
+        if number > total:
+            return -1
+        if number + coins[i] <= total:
+            number += coins[i]
+            count_number += 1
+        else:
+            i += 1
+
+    return count_number
