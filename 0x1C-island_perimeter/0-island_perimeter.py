b61b84a60ce04545c35c8b19cb6248e8e81c9f2f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-13 19:24:28 -0500

    0-island_perimeter.py

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -18,19 +18,26 @@ def island_perimeter(grid):
     """
     perimeter = 0
     for i, row in enumerate(grid):
-        for j, frame in enumerate(row):
-            if frame == 1:
-                if i != len(row) - 1:
+        for j, land in enumerate(row):
+            if land == 1:
+                if i == 0:
+                    perimeter += 1
+                elif grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i < len(grid) - 1:
                     if grid[i + 1][j] == 0:
-                        perimeter +=1
-                if j != len(row) - 1:
-                    if grid[i][j + 1] == 0:
-                        perimeter +=1
+                        perimeter += 1
+                else:
+                    perimeter += 1
                 if j != 0:
                     if grid[i][j - 1] == 0:
-                        perimeter +=1
-                if i != 0:
-                    if grid[i - 1][j] == 0:
-                        perimeter +=1
+                        perimeter += 1
+                else:
+                    perimeter += 1
+                if j < len(row) - 1:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
 
     return perimeter
