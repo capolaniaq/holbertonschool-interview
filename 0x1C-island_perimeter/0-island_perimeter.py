042c6eec8f88a840578fa520de357cdbc7a8f394
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-13 19:15:11 -0500

    0-island_perimeter.py

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Function that calculate the perimeter from island
+
+    Args:
+        grid is a list of list that contain 0's and 1's
+        0 represent the water
+        1 represente the land
+
+    Return:
+        perimeter of the island is the grid
+    """
+    if len(grid) == 0:
+        return 0
+    perimeter = 0
+    for i, row in enumerate(grid):
+        for j, frame in enumerate(row):
+            if frame == 1:
+                if i != len(row) - 1:
+                    if grid[i + 1][j] == 0:
+                        perimeter +=1
+                if j != len(row) - 1:
+                    if grid[i][j + 1] == 0:
+                        perimeter +=1
+                if j != 0:
+                    if grid[i][j - 1] == 0:
+                        perimeter +=1
+                if i != 0:
+                    if grid[i - 1][j] == 0:
+                        perimeter +=1
+
+    return perimeter
