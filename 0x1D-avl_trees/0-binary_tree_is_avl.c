4c5d932eac573ff339221a726541974dd5f8a4bd
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-21 22:47:15 -0500

    0-binary_tree_is_avl.c

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - Check if the binary tree is avl
+ * @tree: is a pointer to root node of the tree to check
+ *
+ * Return: 1 in success case or 0 in failure case
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	return 0;
+}
