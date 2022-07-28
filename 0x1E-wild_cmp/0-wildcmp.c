885f42d6901f4f55fd49fff9c9fbe107a4d007b4
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-28 02:20:51 -0500

    0-wildcmp.c

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -3,30 +3,18 @@
 
 int wildcmp(char *s1, char *s2)
 {
-  int i, j;
-  
-  i = 0;
-  j = 0;
-  
-  while (*(s1 + i) != '\0')
-  {
-    if (*(s2 + j) == '*')
-    {
-      j++;
-      while (*(s1 + i) != s2[j])
-        i++;
-    }
-    else if (*(s1 + i) == *(s2 + j))
-    {
-      i++;
-      j++;
-    }
-    else {
-      return (0);
-    }
-    
-  }
-  
-  return (1);
-  
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
