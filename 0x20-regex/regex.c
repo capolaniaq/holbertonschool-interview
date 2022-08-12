3c4d20dbfab2e5a485db2963e5da0c2e0ac2db14
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-08-11 22:03:12 -0500

    regex.c

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -13,6 +13,9 @@ int regex_match(char const *str, char const *pattern)
 	if (*str == '\0' && *pattern == '\0')
 		return (1);
 
+	if (*str == '\0')
+		return (1);
+
 	if (*str == *pattern)
 		return (regex_match(str + 1, pattern + 1));
 
@@ -20,6 +23,7 @@ int regex_match(char const *str, char const *pattern)
 		return (regex_match(str + 1, pattern + 1));
 
 	if (*pattern == '*')
+	
 		return (regex_match(str + 1, pattern));
 
 	return (0);
