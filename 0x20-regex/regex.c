93dabb861084f904b44ba0329a277ffddb4e9c1d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-08-11 21:47:36 -0500

    regex.c

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,26 @@
+#include "regex.h"
+
+/**
+ * regex_match - checks whether a given pattern matches
+ * a given string
+ * @str: is the string to scan
+ * @pattern: is the regular
+ * Return: int 1 in success or 0 in failure case
+ */
+
+int regex_match(char const *str, char const *pattern)
+{
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+
+	if (*str == *pattern)
+		return (regex_match(str + 1, pattern + 1));
+
+	if (*pattern == '.')
+		return (regex_match(str + 1, pattern + 1));
+
+	if (*pattern == '*')
+		return (regex_match(str + 1, pattern));
+
+	return (0);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,10 @@
+#ifndef _REGEX_H_
+#define _REGEX_H_
+
+#include "stdio.h"
+#include "regex.h"
+#include "stdlib.h"
+
+int regex_match(char const *str, char const *pattern);
+
+#endif
